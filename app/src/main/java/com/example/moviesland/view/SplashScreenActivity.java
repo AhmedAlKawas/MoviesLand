859276ce5666a3a523851d77c720167dcd5f5c73
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-11 21:11:20 +0200

    Designing the splash screen

--- app/src/main/java/com/example/moviesland/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/moviesland/view/SplashScreenActivity.java
@@ -1,14 +1,16 @@
- package com.example.moviesland;
+ package com.example.moviesland.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.moviesland.R;
+
+ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }

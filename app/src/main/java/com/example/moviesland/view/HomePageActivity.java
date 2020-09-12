a1538213b9f67c89582d0f912fc66d42b7f65e80
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-12 03:36:56 +0200

    Implementing get intent extra in the home screen

--- app/src/main/java/com/example/moviesland/view/HomePageActivity.java
+++ app/src/main/java/com/example/moviesland/view/HomePageActivity.java
@@ -1,16 +1,43 @@
 package com.example.moviesland.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moviesland.R;
+import com.example.moviesland.model.Person;
+
+import java.util.List;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    List<Person> personsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        getIntentExtras();
+
     }
+
+    @SuppressWarnings("unchecked")
+    private void getIntentExtras() {
+        if (getIntent().getSerializableExtra(getResources().getString(R.string.people)) != null){
+            personsList = (List<Person>) getIntent().getSerializableExtra(getResources()
+                    .getString(R.string.people));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }

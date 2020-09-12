3ebd9b7a5f1c79979e94d9e841865d2c792373d0
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-12 23:54:07 +0200

    Designing the person details activity

--- app/src/main/java/com/example/moviesland/view/person_details/PersonDetailsActivity.java
+++ app/src/main/java/com/example/moviesland/view/person_details/PersonDetailsActivity.java
@@ -1,16 +1,33 @@
 package com.example.moviesland.view.person_details;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import com.example.moviesland.R;
+import com.example.moviesland.databinding.ActivityPersonDetailsBinding;
+import com.example.moviesland.model.Person;
 
 public class PersonDetailsActivity extends AppCompatActivity {
+
+    ActivityPersonDetailsBinding personDetailsBinding;
+    Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person_details);
+        personDetailsBinding = DataBindingUtil.setContentView(PersonDetailsActivity.this,
+                R.layout.activity_person_details);
+
+        getIntentExtras();
+
+    }
+
+    private void getIntentExtras() {
+        if (getIntent().getSerializableExtra(getString(R.string.person)) != null){
+            person = (Person) getIntent().getSerializableExtra(getString(R.string.person));
+            personDetailsBinding.setPerson(person);
+        }
     }
 }

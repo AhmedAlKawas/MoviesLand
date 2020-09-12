5046ee053f8f61c8bf3c2b00d83df25cef8ca094
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-13 00:11:15 +0200

    Adding get person images url in the service

--- app/src/main/java/com/example/moviesland/network/services/PopularPeopleService.java
+++ app/src/main/java/com/example/moviesland/network/services/PopularPeopleService.java
@@ -1,14 +1,19 @@
 package com.example.moviesland.network.services;
 
+import com.example.moviesland.network.model.GetPersonImagesResponse;
 import com.example.moviesland.network.model.GetPopularPeopleResponse;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PopularPeopleService {
 
     @GET("person/popular?api_key=d1f3680639049343db71b7cfd1181103")
     Observable<GetPopularPeopleResponse> getPopularPeople(@Query("page") int page);
+
+    @GET("person/{personId}/images?api_key=d1f3680639049343db71b7cfd1181103")
+    Observable<GetPersonImagesResponse> getPersonImages(@Path("personId") int personId);
 
 }

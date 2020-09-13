27cb0ac59b4b3e131b945e377840b802f4aa7519
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-13 23:44:22 +0200

    Creating network module

--- app/src/main/java/com/example/moviesland/repository/PeopleRepository.java
+++ app/src/main/java/com/example/moviesland/repository/PeopleRepository.java
@@ -8,35 +8,13 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PeopleRepository {
 
     private PopularPeopleService peopleService;
 
-    private PeopleRepository() {
-
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-        String BASE_URL = "https://api.themoviedb.org/3/";
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
-                .client(client).addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
-        peopleService = retrofit.create(PopularPeopleService.class);
-
-    }
-
-    public static PeopleRepository getInstance() {
-        return Loader.peopleRepository;
-    }
-
-    private static class Loader {
-        static PeopleRepository peopleRepository = new PeopleRepository();
+    public PeopleRepository(PopularPeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     public Observable<GetPopularPeopleResponse> getPopularPeopleRepo(int page) {

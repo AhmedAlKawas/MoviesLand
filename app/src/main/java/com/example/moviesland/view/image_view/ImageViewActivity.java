df5222b010d8c348eafa1fa4e2db8544f955be1a
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-13 03:46:17 +0200

    Getting the intent extra in the image view activity

--- app/src/main/java/com/example/moviesland/view/image_view/ImageViewActivity.java
+++ app/src/main/java/com/example/moviesland/view/image_view/ImageViewActivity.java
@@ -1,16 +1,34 @@
 package com.example.moviesland.view.image_view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import com.example.moviesland.R;
+import com.example.moviesland.databinding.ActivityImageViewBinding;
+import com.example.moviesland.model.Person;
 
 public class ImageViewActivity extends AppCompatActivity {
+
+    ActivityImageViewBinding imageViewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_view);
+        imageViewBinding = DataBindingUtil.setContentView(ImageViewActivity.this,
+                R.layout.activity_image_view);
+
+        getIntentExtras();
+
+    }
+
+    private void getIntentExtras() {
+
+        if (getIntent().getSerializableExtra(getString(R.string.image_path)) != null) {
+            imageViewBinding.setImagePath((String) getIntent()
+                    .getSerializableExtra(getString(R.string.image_path)));
+        }
+
     }
 }

f9591256c93c1ea7cd27c16227f15f317b5bdc48
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-14 01:05:24 +0200

    Implementing context module

---
+++ app/src/main/java/com/example/moviesland/module/ContextModule.java
@@ -0,0 +1,21 @@
+package com.example.moviesland.module;
+
+import android.app.Application;
+import android.content.Context;
+
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class ContextModule {
+
+    private final Context context;
+
+    protected ContextModule(Context context) {
+        this.context = context;
+    }
+
+    @Binds
+    abstract Context bindContext(Application application);
+
+}

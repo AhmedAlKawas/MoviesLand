caf2a427e2be2a8dd818782a760f99c634488254
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-09-14 23:29:13 +0200

    Handling error in the splash screen

--- app/src/main/java/com/example/moviesland/view/splash_screen/SplashScreenActivity.java
+++ app/src/main/java/com/example/moviesland/view/splash_screen/SplashScreenActivity.java
@@ -2,10 +2,14 @@ package com.example.moviesland.view.splash_screen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.moviesland.R;
 import com.example.moviesland.model.Person;
 import com.example.moviesland.view.home_page.HomePageActivity;
@@ -23,6 +27,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Inject
     ViewModelProvider.Factory viewModelFactory;
     private PeopleViewModel peopleViewModel;
+    TextView errorTv;
+    ImageView refreshIv;
+    LottieAnimationView lottieAv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,22 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         initListeners();
+        initViews();
         peopleViewModel.getPopularPeople(1);
+
+    }
+
+    private void initViews() {
+        errorTv = findViewById(R.id.tv_error);
+        refreshIv = findViewById(R.id.iv_refresh);
+        lottieAv = findViewById(R.id.lottie);
+
+        refreshIv.setOnClickListener(view -> {
+            lottieAv.setVisibility(View.VISIBLE);
+            errorTv.setVisibility(View.GONE);
+            refreshIv.setVisibility(View.GONE);
+            peopleViewModel.getPopularPeople(1);
+        });
 
     }
 
@@ -43,7 +65,21 @@ public class SplashScreenActivity extends AppCompatActivity {
         peopleViewModel.returnPersons().observe(SplashScreenActivity.this, response -> {
 
             if (response != null) {
-                goToHomePage(response.getPersonList());
+//                Response success
+                if (response.getCode() == 200)
+                    goToHomePage(response.getPersonList());
+//                SocketTimeoutException or No connection
+                else if (response.getCode() == 501 || response.getCode() == 500) {
+                    lottieAv.setVisibility(View.GONE);
+                    errorTv.setVisibility(View.VISIBLE);
+                    refreshIv.setVisibility(View.VISIBLE);
+                    errorTv.setText(getString(R.string.connection_error));
+//                    Server Error
+                } else {
+                    lottieAv.setVisibility(View.GONE);
+                    errorTv.setVisibility(View.VISIBLE);
+                    errorTv.setText(getString(R.string.server_error));
+                }
             }
 
         });
